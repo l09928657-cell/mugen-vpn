c6c025474978d7b8a47fb2aa38fd71cd1b8185e5
l09928657-cell <l09928657@gmail.com>
2026-06-07 07:52:38 +0000

    Fix applicationId and qt properties

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -17,7 +17,7 @@ val qtTargetAbiList: String by gradleProperties
 val outputBaseName: String by gradleProperties
 
 android {
-    namespace = "org.amnezia.vpn"
+    namespace = "org.mugen.vpn"
 
     buildFeatures {
         viewBinding = true
@@ -34,7 +34,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "org.amnezia.vpn"
+        applicationId = "org.mugen.vpn"
         targetSdk = qtTargetSdkVersion.toInt()
 
         // keeps language resources for only the locales specified below
